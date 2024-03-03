3502dcba69b138f4a0f15c1067d389530d7e6a97
QoidynBasy <c0mplexie1@gmail.com>
2024-03-03 16:31:44 +0600

    new

--- tests/unit.go
+++ tests/unit.go
@@ -4,13 +4,5 @@ import (
 	"testing"
 )
 
-func TestSendMail(t *testing.T, fun error) {
-
-	price := 100
-	discountPercent := 10
-	discountedPrice := CalculateDiscount(price, discountPercent)
-	expectedPrice := 90
-	if discountedPrice != expectedPrice {
-		t.Errorf("Incorrect discounted price. Expected: %d, Got: %d", expectedPrice, discountedPrice)
-	}
+func TestSendMail(t *testing.T) {
 }
